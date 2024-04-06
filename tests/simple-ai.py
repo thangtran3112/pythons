22a13cfd63dd31b14879c1b67fe1d2bc6c10d736
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-04-06 13:01:25 -0700

    more python error handling

---
+++ tests/simple-ai.py
@@ -0,0 +1,15 @@
+def interac():
+    while True:
+        try:
+            user_input = int(input("Please enter an integer: "))
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+        else:
+            print(
+                "{} is {}".format(user_input, "even" if user_input % 2 == 0 else "odd")
+            )
+        finally:
+            user_input = input("Do you want to play again? (y/n) ")
+            if user_input != "y":
+                print("Goodbye!")
+                break
